3b5584625510a433779a8c78b28a71848ce8f47e
fcancelinha <fmcancelinha@gmail.com>
2020-08-17 18:52:54 +0100

    Recursos do backoffice

--- PROJECTOFINAL/User.cs
+++ PROJECTOFINAL/User.cs
@@ -12,14 +12,14 @@ namespace PROJECTOFINAL
         public static bool isLogged { get; set; }
 
         //User Object Details
-        public int userID { get; set; }
-        public string name { get; set; }
-        public string email { get; set; }
-        public string address { get; set; }
-        public string NIF { get; set; }
-        public string nrSaude { get; set; }
-        public char gender { get; set; }
-        public DateTime birthday { get; set; }
+        public static int userID { get; set; }
+        public static string name { get; set; }
+        public static string email { get; set; }
+        public static string address { get; set; }
+        public static string NIF { get; set; }
+        public static string nrSaude { get; set; }
+        public static char gender { get; set; }
+        public static DateTime birthday { get; set; }
 
     }
 }
