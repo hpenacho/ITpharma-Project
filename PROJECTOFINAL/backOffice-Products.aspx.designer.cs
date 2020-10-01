bbfcc0ccc4d73eee3e882a10fe3a3e9a19701a92
fcancelinha <fmcancelinha@gmail.com>
2020-10-01 15:09:42 +0100

    update panel insert product

--- PROJECTOFINAL/backOffice-Products.aspx.designer.cs
+++ PROJECTOFINAL/backOffice-Products.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace PROJECTOFINAL
         protected global::System.Web.UI.WebControls.Repeater Repeater1;
 
         /// <summary>
+        /// updInsertPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updInsertPanel;
+
+        /// <summary>
         /// tb_name control.
         /// </summary>
         /// <remarks>
