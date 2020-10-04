f8c96399e90e9b34f6f12f08c7ef66ce7ed27e4b
fcancelinha <fmcancelinha@gmail.com>
2020-10-04 17:41:04 +0100

    Revert "merge fixes"
    This reverts commit 2485562505d6e8bae3c7dbb93b9041a749868a4d.

---
+++ PROJECTOFINAL/ATM-pickupChoice.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PROJECTOFINAL
+{
+    public partial class ATM_pickupChoice : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lbtn_loadChosenATM_Click(object sender, EventArgs e)
+        {
+
+            ATM.setATM(Convert.ToInt32(ddl_ATMchoice.SelectedValue), ddl_ATMchoice.SelectedItem.Text);
+
+            Response.Redirect("ATM-Front.aspx");
+        }
+    }
+}
